530ceeec2270abdfc7589f9891a3e00ca3f614c1
RaulAle119 <raul.gonzalezsvr@uanl.edu.mx>
2024-09-13 13:03:34 -0600

    Change
    Se agrego una funcion nueva al script de  para consultar la API

--- PIA_PC/ApiCalis.psm1
+++ PIA_PC/ApiCalis.psm1
@@ -1,14 +1,40 @@
 function Get-VirusTotalReport {
     param ([string]$ApiKey)
-    Write-Host "Ingrese el Hash que desea analizar"
+    
+    Write-Host "Ingresa el Hash que desea analizar"
     $Hash = Read-Host
     $uri = "https://www.virustotal.com/api/v3/files/$Hash"
     $headers = @{
         "x-apikey" = "b5323ef020ab7a5600f682882c05417d58603b4e725d55841dab72902f76be59"
     }
     
-    $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    #return $response
-    $results = $response.data.attributes.last_analysis_results
-    return $results
+    try {
+        $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+        $results = $response.data.attributes.last_analysis_results | Format-List
+        return $results
+    }
+    catch {
+        Write-Host "Ocurrio un error al obtener el reporte de VirusTotal: $_"
+    }
+    function Scan-FilesInFolder {
+        param ([string]$FolderPath, [string]$ApiKey)
+        $files = Get-ChildItem -Path $FolderPath -File
+        foreach ($file in $files) {
+            try {
+                $hash = Get-FileHash -Path $file.FullName -Algorithm SHA256
+            
+                $url = "https://www.virustotal.com/vtapi/v2/file/report?apikey=$ApiKey&resource=$($hash.Hash)"
+                $response = Invoke-RestMethod -Uri $url -Method Get
+
+                if ($response.response_code -eq 1) {
+                    Write-Output "$($file.Name): $($response.positives)/$($response.total) antivirus lo marcaron como malicioso."
+                } else {
+                    Write-Output "$($file.Name): El archivo no se encuentra base de datos de VirusTotal."
+                }
+            }
+            catch {
+                Write-Output "Ocurrio un error con $($file.Name): $_"
+            }
+        }
+    }
 }
---
+++ PIA_PC/Arch_Oc.ps1
@@ -0,0 +1,38 @@
+﻿#Cambiamos la politica de ejecucion
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+#Asignamos una funcion la cual servira para buscar los archivos ocultos en la ruta deseada
+function archivos_ocultos { param ([string]$ruta)
+
+#Aqui escribimos unicamente el nombre de la carpeta, no la ruta
+$carpeta = Read-Host "Por favor, proporciona el nombre de la carpeta a buscar"
+
+#Buscara en todo el equipo el nombre de la carpeta
+$directorio_raiz = "C:\"
+
+$buscar = Get-ChildItem -Path $directorio_raiz -Recurse -Directory -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq $carpeta }
+
+#Dividimos en 2 posibles opciones, si encontro una carpeta con el nombre que pedimos
+if ($buscar) {
+    #Al encontrar una busqueda similar, imprimira todos los nombres de las carpetas que tengan en su direccion el nombre de la carpeta
+    Write-Host "Se encontro la carpeta:"
+    $buscar | ForEach-Object { Write-Host $_.FullName }
+    $buscar | ForEach-Object { $ruta = $_.FullName }
+    write-host $ruta
+    #Busca los archivos ocultos dentro de la carpeta encontrada
+        Write-Host "Buscando archivos ocultos en la carpeta $ruta"
+        $busqueda = Get-ChildItem -Path $ruta -Recurse -Force | Where-Object { $_.Attributes -match 'Hidden' }
+        #Si encuentra algun archivo oculto en la carpeta, nos indicara cuales son
+        if ($busqueda) {
+            Write-Host "Archivos ocultos encontrados:"
+            $busqueda | ForEach-Object { Write-Host $_.FullName }
+        } else {
+        #En caso de que no, nos avisara que no hay ninguno
+            Write-Host "No se encontraron archivos ocultos en $ruta."
+        }
+#Al momento de escribir el nombre de la carpeta y no exista, nos aparecera este mensaje de que no hubo busquedas relacionadas
+} else {
+    Write-Host "No se encontraron carpetas con el nombre '$carpeta' en $directorio_raiz."
+}
+}
+#Se imprime la funcion
+archivos_ocultos 
